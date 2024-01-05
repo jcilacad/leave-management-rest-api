f447b1492752e3cd185d8ac90bd33d1d69865cbd
jcilacad <johnchristopherilacad27@gmail.com>
2024-01-05 21:19:54 +0800

    Initialize commit.

---
+++ src/main/java/com/api/management/leave/leavemanagementapi/LeaveManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.management.leave.leavemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagementApiApplication.class, args);
+	}
+
+}
