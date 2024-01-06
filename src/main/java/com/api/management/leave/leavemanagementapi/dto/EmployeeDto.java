a70fe98faf3cf7ae46ff0bcc3422bda6bdc08bd5
jcilacad <johnchristopherilacad27@gmail.com>
2024-01-06 16:08:31 +0800

    Add validations for employeeDto.

--- src/main/java/com/api/management/leave/leavemanagementapi/dto/EmployeeDto.java
+++ src/main/java/com/api/management/leave/leavemanagementapi/dto/EmployeeDto.java
@@ -2,14 +2,36 @@ package com.api.management.leave.leavemanagementapi.dto;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDto {
+    private Long id;
+    @NotEmpty
+    private String employeeNumber;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String middleName;
+    @NotEmpty
+    private String lastName;
+    private String nameExtension;
+    @NotEmpty
+    @Email
+    private String officialEmail;
+    private BigDecimal forcedLeave;
+    private BigDecimal specialPrivilegeLeave;
+    private BigDecimal vacationLeave;
+    private BigDecimal sickLeave;
+    private BigDecimal leaveWithoutPay;
 }
