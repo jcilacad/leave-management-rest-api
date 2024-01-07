982380cd7053ba444f5c1930ace48c7c80b6f33e
jcilacad <johnchristopherilacad27@gmail.com>
2024-01-07 16:25:02 +0800

    Add pagination for employees.

--- src/main/java/com/api/management/leave/leavemanagementapi/controller/EmployeeController.java
+++ src/main/java/com/api/management/leave/leavemanagementapi/controller/EmployeeController.java
@@ -1,14 +1,13 @@
 package com.api.management.leave.leavemanagementapi.controller;
 
 import com.api.management.leave.leavemanagementapi.dto.EmployeeDto;
+import com.api.management.leave.leavemanagementapi.dto.EmployeeResponse;
 import com.api.management.leave.leavemanagementapi.service.EmployeeService;
+import com.api.management.leave.leavemanagementapi.utils.AppConstants;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/employees")
@@ -19,8 +18,18 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @GetMapping
+    public ResponseEntity<EmployeeResponse> getAllEmployees(
+            @RequestParam(name = "pageNo", required = false, defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int pageNo,
+            @RequestParam(name = "pageSize", required = false, defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int pageSize,
+            @RequestParam(name = "sortBy", required = false, defaultValue = AppConstants.DEFAULT_SORT_BY) String sortBy,
+            @RequestParam(name = "sortDir", required = false, defaultValue = AppConstants.DEFAULT_SORT_DIR) String sortDir) {
+        return ResponseEntity.ok(employeeService.getAllEmployees(pageNo, pageSize, sortBy, sortDir));
+    }
+
+
     @PostMapping
-    public ResponseEntity<EmployeeDto> createEmployee (@RequestBody @Valid EmployeeDto employeeDto) {
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody @Valid EmployeeDto employeeDto) {
         return new ResponseEntity<>(employeeService.createEmployee(employeeDto), HttpStatus.CREATED);
     }
 }
